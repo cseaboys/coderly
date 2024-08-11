bfc045d56225d9a40bb21937ac41e18a9c180730
aber-coder <32mftgfm@duck.com>
2024-08-11 20:09:27 +0530

    Update checksum.py

--- checksum.py
+++ checksum.py
@@ -1,27 +1,32 @@
 import sys
 from typing import List
-#redirect input 
-sys.stdin = open("input.txt","r")
-sys.stdout = open("output.txt","w")
- 
-buff :str = input()
-s :int = int(input())
 
-#chunk and sum data
-chunks :List[int] = [int("0b" + buff[i:i+s],2) for i in range(0,len(buff),s)]
-sum_of_chunks :str = bin(sum(chunks)) 
-print("Chunks: ", chunks)
+# redirect input
+sys.stdin = open("input.txt", "r")
+# 10101111
+# 4
+sys.stdout = open("output.txt", "w")
+
+buff: str = input()
+s: int = int(input())
+
+# chunk and sum data
+chunks: List[int] = ["0b" + buff[i : i + s] for i in range(0, len(buff), s)]
+sum_of_chunks: str = bin(sum(map(lambda x: int(x, 2), chunks)))
+print("Chunks: ", *chunks)
 print(f"{sum_of_chunks=}")
 
-def get_complement(buff :str):
-    buff = buff[2:] #discard 0b at the start
-    return "0b"+"".join("1" if bit == "0" else "0" for bit in buff)
+
+def get_complement(buff: str):
+    buff = buff[2:]  # discard 0b at the start
+    return "0b" + "".join("1" if bit == "0" else "0" for bit in buff)
+
 
 complement_of_chunks_sum = get_complement(sum_of_chunks)
 print(f"{complement_of_chunks_sum=}")
 
-sum_of_chunks_and_complement = bin(int(sum_of_chunks,2)+int(complement_of_chunks_sum,2))
+sum_of_chunks_and_complement = bin(
+    int(sum_of_chunks, 2) + int(complement_of_chunks_sum, 2)
+)
 print(f"{sum_of_chunks_and_complement=}")
-print(f"{get_complement(sum_of_chunks_and_complement)=}")
-
-
+print(f"{int(get_complement(sum_of_chunks_and_complement),2) == 0 =}")

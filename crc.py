68c75b863121a15694c8d7b493ae4629154c7b49
aber-coder <32mftgfm@duck.com>
2024-09-27 23:28:55 +0530

    Update crc.py

--- crc.py
+++ crc.py
@@ -1,17 +1,5 @@
-def crc(mx, gx):
-    pad_len = len(gx) - 1
-    dividend = int(mx + "0" * pad_len, 2)
-    divisor = int(gx, 2)
-    remainder = bin((dividend % divisor) + 1)[2:]
+remainder = dividend
 
-    print(f"{remainder=}")
-    encoded_data = mx + remainder
-    print(f"{encoded_data=}")
-
-    received_data_properly = int(encoded_data, 2) % divisor == 0
-    print(f"{received_data_properly= }")
-
-
-mx = "101011"
-gx = "1101"
-crc(mx, gx)
+while remainder.bit_length() >= divisor.bit_length():
+    shift = remainder.bit_length() - divisor.bit_length()
+    remainder ^= divisor << shift

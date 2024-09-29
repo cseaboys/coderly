0051081ba250231e941e1ff698390d0acd6ab8b5
aber-coder <32mftgfm@duck.com>
2024-09-29 19:25:11 +0530

    update crc.py

--- crc.py
+++ crc.py
@@ -1,5 +1,13 @@
+mx = input("Enter message (binary): ")
+gx = input("Enter generator (binary): ")
+
+dividend = int(mx, 2)
+divisor = int(gx, 2)
+
 remainder = dividend
 
 while remainder.bit_length() >= divisor.bit_length():
     shift = remainder.bit_length() - divisor.bit_length()
     remainder ^= divisor << shift
+
+print(f"CRC remainder: {bin(remainder)[2:]}")

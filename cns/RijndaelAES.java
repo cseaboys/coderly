da73679a58c923517ad7365c693e93e1bf3eb3b3
cseaboys <cseaboys2026@gmail.com>
2025-03-15 05:19:00 +0000

    add rijandaelaes.java

---
+++ cns/RijndaelAES.java
@@ -0,0 +1,40 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+
+public class RijndaelAES {
+    public static void main(String[] args) {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128); // AES key size (128, 192, or 256 bits)
+            SecretKey secretKey = keyGenerator.generateKey();
+
+            String plaintext = "This is a Rijndael AES logic!";
+            System.out.println("Original Text: " + plaintext);
+
+            String encryptedText = encrypt(plaintext, secretKey);
+            System.out.println("Encrypted Text: " + encryptedText);
+
+            String decryptedText = decrypt(encryptedText, secretKey);
+            System.out.println("Decrypted Text: " + decryptedText);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String encrypt(String plaintext, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedText, SecretKey secretKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        return new String(decryptedBytes);
+    }
+}

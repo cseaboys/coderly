e1d92ff8f725c95af0fb425b9123cfac43b61950
cseaboys <cseaboys2026@gmail.com>
2025-03-29 11:03:53 +0530

    some enhancements

--- cns/P6_rijndaelaes.java
+++ cns/P6_rijndaelaes.java
@@ -1,40 +1,32 @@
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+
 import java.util.Base64;
 
 public class P6_rijndaelaes {
     public static void main(String[] args) {
         try {
             KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-            keyGenerator.init(128); 
+            keyGenerator.init(128);
             SecretKey secretKey = keyGenerator.generateKey();
-
-            String plaintext = "This is a CODE";
+            
+            String plaintext = "Hello world";
             System.out.println("Original Text: " + plaintext);
-
-            String encryptedText = encrypt(plaintext, secretKey);
+            
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            String encryptedText = Base64.getEncoder().encodeToString(
+                cipher.doFinal(plaintext.getBytes()));
             System.out.println("Encrypted Text: " + encryptedText);
-
-            String decryptedText = decrypt(encryptedText, secretKey);
+            
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            String decryptedText = new String(
+                cipher.doFinal(Base64.getDecoder().decode(encryptedText)));
             System.out.println("Decrypted Text: " + decryptedText);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    public static String encrypt(String plaintext, SecretKey secretKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedBytes);
-    }
-
-    public static String decrypt(String encryptedText, SecretKey secretKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("AES");
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
-        return new String(decryptedBytes);
     }
 }
--- cns/P8_rsa.java
+++ cns/P8_rsa.java
@@ -1,53 +1,38 @@
-import java.util.*;
-import java.math.*;
+import java.util.Scanner;
+import java.math.BigInteger;
 
 class P8_rsa {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        int p, q, n, z, d = 0, e, i;
-        System.out.println("Enter the number to be encrypted and decrypted");
+
+        System.out.println("Enter number to encrypt/decrypt:");
         int msg = sc.nextInt();
-        double c;
-        BigInteger msgback;
-        System.out.println("Enter 1st prime number p");
-        p = sc.nextInt();
-        System.out.println("Enter 2nd prime number q");
-        q = sc.nextInt();
+        System.out.println("Enter 1st prime number p:");
+        int p = sc.nextInt();
+        System.out.println("Enter 2nd prime number q:");
+        int q = sc.nextInt();
 
-        n = p * q;
-        z = (p - 1) * (q - 1);
-        System.out.println("the value of z = " + z);
+        int n = p * q;
+        int z = (p - 1) * (q - 1);
+        int e = 2;
+        while (gcd(e, z) != 1)
+            e++;
 
-        for (e = 2; e < z; e++) {
-            if (gcd(e, z) == 1) {
-                break;
-            }
-        }
-        System.out.println("the value of e = " + e);
-        for (i = 0; i <= 9; i++) {
-            int x = 1 + (i * z);
-            if (x % e == 0) {
-                d = x / e;
-                break;
-            }
-        }
-        System.out.println("the value of d = " + d);
-        c = (Math.pow(msg, e)) % n;
-        System.out.println("Encrypted message is : -");
-        System.out.println(c);
+        int d = 0;
+        while ((1 + d * z) % e != 0)
+            d++;
+        d = (1 + d * z) / e;
+
+        System.out.println("z = " + z + ", e = " + e + ", d = " + d);
+        double c = Math.pow(msg, e) % n;
+        System.out.println("Encrypted message: " + c);
+
         BigInteger N = BigInteger.valueOf(n);
-        BigInteger C = BigDecimal.valueOf(c).toBigInteger();
-        msgback = (C.pow(d)).mod(N);
-        System.out.println("Derypted message is : -");
-        System.out.println(msgback);
-
+        BigInteger C = BigInteger.valueOf((long) c);
+        System.out.println("Decrypted message: " + C.pow(d).mod(N));
     }
 
     static int gcd(int e, int z) {
-        if (e == 0)
-            return z;
-        else
-            return gcd(z % e, e);
+        return e == 0 ? z : gcd(z % e, e);
     }
-
 }
